5889948ba95201a2c92592e7d22561464206a1fd
Pedro David López Correa <pedro_corra23@hotmail.com>
2023-02-22 00:01:50 -0600

    Complete Section 9 | PaisesApp

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -6,7 +6,11 @@ import { Country } from '../../interfaces/pais.interfaces';
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  styles: [
+  styles: [`
+    li{
+      cursor: pointer;
+    }
+  `
   ]
 })
 export class PorPaisComponent {
@@ -14,6 +18,9 @@ export class PorPaisComponent {
   termino: string = ''
   hayError: boolean = false;
   paises: Country[] = [];
+
+  paisesSugeridos: Country[] = [];
+  mostrarSugerencias: boolean = false;
 
   constructor ( private PaisService: PaisService ) {}
 
@@ -39,7 +46,16 @@ export class PorPaisComponent {
 
   sugerencias( termino: string) {
     this.hayError = false;
-    
+    this.termino = termino;
+    this.mostrarSugerencias = true;
+
+    this.PaisService.buscarPais ( termino )
+      .subscribe({ next: (paises) => {this.paisesSugeridos = paises.splice(0,3)},
+                   error: (err) => {this.paisesSugeridos = []}} );
+  }
+
+  buscarSugerido ( termino:string ) {
+      this.buscar( termino );
   }
 
 }
--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
 
+import { Country } from '../../interfaces/pais.interfaces';
+import { PaisService } from '../../services/pais.service';
+
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styles: [
+  styles: [`
+    button{
+      margin-right: 5px;
+    }
+  `
   ]
 })
 export class PorRegionComponent {
+
+  regiones: string [] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regionActiva: string = '';
+  region: Country[] = [];
+
+  constructor ( private PaisService: PaisService ) {}
+
+  getClaseCSS (region: string): string {
+    return (region === this.regionActiva) ? 'btn btn-primary': 'btn btn-outline-primary'
+  }
+
+  activarRegion ( region: string ) {
+    if ( region === this.regionActiva ) { return }
+    this.regionActiva = region;
+    this.PaisService.buscarRegion ( region )
+      .subscribe( region => this.region = region );
+  }
 
 }
